9765289d3ed6e8a69914e3bdc16ed0e43a83d3e5
Ruth Mcilwaine <53459826+RuthMcilwaine@users.noreply.github.com>
2025-01-27 16:58:08 +1100

    Delete helloreact directory

--- helloreact/src/components/Greeting.js
+++
@@ -1,9 +0,0 @@
-//functional component
-function Greeting(props) {
-  //let name = 'Dhitaj';
-  // inspect on chrome console
-  console.log(props);
-  return <h1>Hello from {props.firstName}</h1>;
-}
-
-export default Greeting;
--- helloreact/src/components/Greeting1.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-//class component
-export default class Greeting1 extends Component {
-  render() {
-    return <div>Hello from {this.props.firstName} Component</div>;
-  }
-}
